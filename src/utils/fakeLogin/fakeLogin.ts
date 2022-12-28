c9fd36ca37c3869e2a652783d8765d14f179e974
Vinicius <vinicius.benedito@cuponeria.com.br>
2022-12-28 07:35:02 -0300

    feat: create reject if email or password is wrong

--- src/utils/fakeLogin/fakeLogin.ts
+++ src/utils/fakeLogin/fakeLogin.ts
@@ -6,10 +6,12 @@ interface FakeLoginProps {
 export function fakeLogin({ email, password }: FakeLoginProps) {
   const delay = (0.7 + Math.random() * 2) * 1000;
 
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
     setTimeout(function () {
       if (password === "password123" && !!email) {
         resolve(true);
+      } else {
+        reject({ error: "e-mail or password wrong." });
       }
     }, delay);
   });
