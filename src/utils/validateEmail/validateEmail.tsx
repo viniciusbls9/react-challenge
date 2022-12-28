09d4ca07bc0e567db4fc1c2e64615e7dbf48645e
Vinicius <vinicius.benedito@cuponeria.com.br>
2022-12-28 07:52:11 -0300

    feat: return false if regEx test isn't valid

--- src/utils/validateEmail/validateEmail.tsx
+++ src/utils/validateEmail/validateEmail.tsx
@@ -2,6 +2,8 @@ function validateEmail(email: string) {
   const regEx = new RegExp(/^[a-z0-9.]+@[a-z0-9]+.[a-z]+.([a-z]+)?$/i);
 
   if (regEx.test(email)) return true;
+
+  return false;
 }
 
 export default validateEmail;
