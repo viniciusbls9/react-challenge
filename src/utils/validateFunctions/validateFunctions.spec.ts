f32dcf15678167695932e93fc888cc8fb46332ad
Vinicius <vinicius.benedito@cuponeria.com.br>
2022-12-28 21:06:39 -0300

    feat: create new project structure

---
+++ src/utils/validateFunctions/validateFunctions.spec.ts
@@ -0,0 +1,26 @@
+import { validateEmail, validatePassword } from "./validateFunctions";
+
+describe("Validade Email function", () => {
+  test("should return true if email is valid on regEx test", () => {
+    const email = "beneditoura@gmail.com";
+    const validate = validateEmail(email);
+
+    expect(validate).toBe(true);
+  });
+
+  test("should return false if email isn't valid on regEx test", () => {
+    const email = "beneditoura";
+    const validate = validateEmail(email);
+
+    expect(validate).toBe(false);
+  });
+});
+
+describe("Validade Password function", () => {
+  test("should return true if password is valid on regEx test", () => {
+    const password = "Oi123#!@";
+    const validate = validatePassword(password);
+
+    expect(validate).toBe(true);
+  });
+});
---
+++ src/utils/validateFunctions/validateFunctions.ts
@@ -0,0 +1,15 @@
+export function validateEmail(email: string) {
+  const regEx = new RegExp(/^[a-z0-9.]+@[a-z0-9]+.[a-z]+.([a-z]+)?$/i);
+
+  if (regEx.test(email)) return true;
+
+  return false;
+}
+
+export function validatePassword(password: string): boolean {
+  const regEx = new RegExp(/^[a-z0-9.]+@[a-z0-9]+.[a-z]+.([a-z]+)?$/i);
+
+  if (regEx.test(password)) return true;
+
+  return false;
+}
