a1c54e3bb42045f03dffce2d5db1505f216417a6
Vinicius <vinicius.benedito@cuponeria.com.br>
2022-12-28 21:23:10 -0300

    feat: create regEx to validate password function

--- src/utils/validateFunctions/validateFunctions.ts
+++ src/utils/validateFunctions/validateFunctions.ts
@@ -6,10 +6,10 @@ export function validateEmail(email: string) {
   return false;
 }
 
-export function validatePassword(password: string): boolean {
-  const regEx = new RegExp(/^[a-z0-9.]+@[a-z0-9]+.[a-z]+.([a-z]+)?$/i);
+export function validatePassword(password: string) {
+  const regEx = new RegExp(
+    /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/
+  );
 
   if (regEx.test(password)) return true;
-
-  return false;
 }
