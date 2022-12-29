90809c9d3e1d02542017f6d72b9b04dd7138f4d0
Vinicius <vinicius.benedito@cuponeria.com.br>
2022-12-29 11:34:44 -0300

    test: change fake loginData

--- src/utils/fakeLogin/fakeLogin.spec.ts
+++ src/utils/fakeLogin/fakeLogin.spec.ts
@@ -4,16 +4,16 @@ describe("fakeLogin function", () => {
   test("should resolve promise if email and password are correct", async () => {
     const loginData = {
       email: "beneditoloura@gmail.com",
-      password: "password123",
+      password: "Passw@rd123",
     };
 
     await expect(fakeLogin(loginData)).resolves.toBe(true);
   });
 
-  test("should reject promise if email and password are wrong", async () => {
+  test("should reject promise if email or password are wrong", async () => {
     const loginData = {
       email: "",
-      password: "password1",
+      password: "1",
     };
 
     await expect(fakeLogin(loginData)).rejects.toMatchObject({
