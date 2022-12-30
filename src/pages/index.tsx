0e777966aee1edb95fb590d2b3f5c2e1c3e4e502
Vinicius <beneditoloura@gmail.com>
2022-12-30 07:26:44 -0300

    feat: create states to manipulate inputs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,32 @@
 import { fakeLogin } from "../utils/fakeLogin/fakeLogin";
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 import Button from "../components/Button";
 
-// Instruções:
-// * Você tem um formulário de login INCOMPLETO
-// * Não é permitido adicionar novos elementos HTML
-// * Não é permitido usar refs
-//
 // Tarefas:
-// todo - O botão de login deve disparar a função login(), importada no topo deste arquivo, e passar os dados necessários.
-// todo - Desabilite o botão de Login caso o e-mail esteja em branco OU a senha for menor que 6 dígitos.
-// todo - Desabilite o botão de Login equanto você está executando o login.
-// todo - Mostre uma mensagem de erro de login() caso o Login falhe. A mensagem deve ser limpa a cada nova tentativa de Login.
-// todo - Mostre um alerta caso o login seja efetuado com sucesso (javascript alert). Investigue a função login() para entender como ter sucesso na requisição.
+// [x] - O botão de login deve disparar a função login(), importada no topo deste arquivo, e passar os dados necessários.
+// [] - Desabilite o botão de Login caso o e-mail esteja em branco OU a senha for menor que 6 dígitos.
+// [] - Desabilite o botão de Login equanto você está executando o login.
+// [] - Mostre uma mensagem de erro de login() caso o Login falhe. A mensagem deve ser limpa a cada nova tentativa de Login.
+// [] - Mostre um alerta caso o login seja efetuado com sucesso (javascript alert). Investigue a função login() para entender como ter sucesso na requisição.
 
 export default function LoginForm() {
+  const [loginData, setLoginData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleEmail = (event: ChangeEvent<HTMLInputElement>) => {
+    const value = event.target?.value;
+
+    setLoginData({ ...loginData, email: value });
+  };
+
+  const handlePassword = (event: ChangeEvent<HTMLInputElement>) => {
+    const value = event.target?.value;
+
+    setLoginData({ ...loginData, password: value });
+  };
+
   return (
     <div className="wrapper">
       <div className="login-form">
@@ -23,15 +35,26 @@ export default function LoginForm() {
         <div className="errorMessage"></div>
         <div className="row">
           <label htmlFor={"email"}>Email</label>
-          <input id={"email"} type={"email"} autoComplete="off" />
+          <input
+            id={"email"}
+            type={"email"}
+            value={loginData.email}
+            onChange={handleEmail}
+            autoComplete="off"
+          />
         </div>
         <div className="row">
           <label htmlFor={"password"}>Password</label>
-          <input id={"password"} type={"password"} />
+          <input
+            id={"password"}
+            type={"password"}
+            value={loginData.email}
+            onChange={handlePassword}
+          />
         </div>
 
         <div className="button">
-          <Button label="Login" />
+          <Button label="Login" onClick={() => fakeLogin(loginData)} />
         </div>
       </div>
     </div>
