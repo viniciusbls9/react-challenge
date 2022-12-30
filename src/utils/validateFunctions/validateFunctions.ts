4aea58a9539dc2ee2b24f9e99d06cd16acea90ea
Vinicius <beneditoloura@gmail.com>
2022-12-30 07:43:02 -0300

    feat: return false if email is empty

--- src/utils/validateFunctions/validateFunctions.ts
+++ src/utils/validateFunctions/validateFunctions.ts
@@ -1,4 +1,6 @@
 export function validateEmail(email: string) {
+  if (email === "") return false;
+
   const regEx = new RegExp(/^[a-z0-9.]+@[a-z0-9]+.[a-z]+.([a-z]+)?$/i);
 
   if (regEx.test(email)) return true;
