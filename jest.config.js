53ccc9dc228a0e309f86d8f64a33581e94e346d0
Vinicius <beneditoloura@gmail.com>
2023-01-05 22:07:38 -0300

    feat: update alias path

--- jest.config.js
+++ jest.config.js
@@ -13,6 +13,7 @@ const customJestConfig = {
     '^@/components/(.*)$': '<rootDir>/src/components/$1',
 
     '^@/pages/(.*)$': '<rootDir>/src/pages/$1',
+    '^@/utils/(.*)$': '<rootDir>/src/utils/$1',
   },
   testEnvironment: 'jest-environment-jsdom',
 }
